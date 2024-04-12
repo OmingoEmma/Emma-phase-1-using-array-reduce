de835eb361f5657529a59a73c0c02393fba27cd6
OmingoEmma <emmaomingo@gmail.com>
2024-04-12 04:39:08 +0300

    Complete Lab

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const batteries = [
+    {
+      id: 101,
+      name: 'Battery A',
+      assembled: true,
+      quantity: 5
+    },
+    {
+      id: 102,
+      name: 'Battery B',
+      assembled: false,
+      quantity: 10
+    },
+    {
+      id: 103,
+      name: 'Battery C',
+      assembled: true,
+      quantity: 16
+    }
+  ];
+  
+  const totalBatteries = batteries.reduce((acc, battery) => {
+    if (battery.assembled) {
+      return acc + battery.quantity;
+    } else {
+      return acc;
+    }
+  }, 0);
+  
+ 
